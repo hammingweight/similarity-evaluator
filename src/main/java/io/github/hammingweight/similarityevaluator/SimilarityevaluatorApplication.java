11e3058281d78c34c21427f0da2d18bca7734647
Carl Meijer <hammingweight@gmail.com>
2025-12-03 09:35:40 +0200

    Add skeleton code from Spring initializr.

---
+++ src/main/java/io/github/hammingweight/similarityevaluator/SimilarityevaluatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.hammingweight.similarityevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityevaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarityevaluatorApplication.class, args);
+	}
+
+}
