82ebb74faeb0d687b7b5aa9afc082712f8fbcdde
Carl Meijer <hammingweight@gmail.com>
2025-12-04 14:35:51 +0200

    Improve test readability.

--- src/test/java/io/github/hammingweight/similarityevaluator/SimilarityEvaluatorIntegrationTests.java
+++ src/test/java/io/github/hammingweight/similarityevaluator/SimilarityEvaluatorIntegrationTests.java
@@ -12,42 +12,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @SpringBootApplication
 public class SimilarityEvaluatorIntegrationTests {
-	
+
 	@Autowired
 	public EmbeddingModel embeddingModel;
 
-	
 	@Test
 	public void testGoodSimilarity() {
 		SimilarityEvaluator evaluator = new SimilarityEvaluator(embeddingModel);
+
 		EvaluationRequest request = new EvaluationRequest("the cat sat on the mat", "the cat sat on the mat");
 		EvaluationResponse response = evaluator.evaluate(request);
+
 		Assertions.assertTrue(response.isPass());
 		Assertions.assertEquals(1.0, response.getScore(), 0.001);
 	}
-	
+
 	@Test
 	public void testBadSimilarity() {
 		SimilarityEvaluator evaluator = new SimilarityEvaluator(embeddingModel, 0.95);
+
 		EvaluationRequest request = new EvaluationRequest("the cat sat on the mat", "llms are strange");
 		EvaluationResponse response = evaluator.evaluate(request);
+
 		Assertions.assertFalse(response.isPass());
 		Assertions.assertTrue(response.getScore() < 0.9);
 	}
-	
+
 	@Test
 	public void testReasonableSimilarity() {
-		String actual = "The French capital city is Paris";
-		String expected = "The capital of France is Paris";
-		String badResponse = "The French capital city is Munich";
 		SimilarityEvaluator evaluator = new SimilarityEvaluator(embeddingModel);
-		EvaluationRequest request = new EvaluationRequest(expected, actual);
-		EvaluationResponse response = evaluator.evaluate(request);
-		float goodSimilarity = response.getScore();
-		request = new EvaluationRequest(actual, badResponse);
-		response = evaluator.evaluate(request);
-		float badSimilarity = response.getScore();
-		Assertions.assertTrue(goodSimilarity > badSimilarity);
+
+		String expected = "The French capital city is Paris";
+		String goodResponse = "The capital of France is Paris";
+		String badResponse = "The French capital city is Munich";
+
+		EvaluationRequest goodRequest = new EvaluationRequest(expected, goodResponse);
+		EvaluationResponse good = evaluator.evaluate(goodRequest);
+
+		EvaluationRequest badRequest = new EvaluationRequest(expected, badResponse);
+		EvaluationResponse bad = evaluator.evaluate(badRequest);
+
+		Assertions.assertTrue(good.getScore() > bad.getScore());
 	}
 
 }
